2f6e87367ead5a5bd6d557e1519179ae6919bae1
coolcute <coolcute@a8a43c1e-1b8f-11de-b56b-c9554f25afe1>
2009-04-08 11:30:16 +0000

    修改网卡列表的显示方式

--- trunk/h3c_svr/main.cpp
+++ trunk/h3c_svr/main.cpp
@@ -7,6 +7,8 @@
 
 #include "utils.h"
 
+using namespace std;
+
 ////////////////////////////////////////////////////////////
 // Declare several global variables to share 
 // their values across multiple functions of your program.
@@ -278,153 +280,36 @@ bool StartMyService ()
 	return StartServiceCtrlDispatcher(ServiceTable) != FALSE;
 }
 
-#define MALLOC(x) HeapAlloc(GetProcessHeap(), 0, (x))
-#define FREE(x) HeapFree(GetProcessHeap(), 0, (x))
-/* Note: could also use malloc() and free() */
-
 bool ListDevice ()
 {
-	/* Declare and initialize variables */
+	pcap_if_t *alldevs = 0;
+	char errbuf[PCAP_ERRBUF_SIZE];
 
-	// It is possible for an adapter to have multiple
-	// IPv4 addresses, gateways, and secondary WINS servers
-	// assigned to the adapter. 
-	//
-	// Note that this sample code only prints out the 
-	// first entry for the IP address/mask, and gateway, and
-	// the primary and secondary WINS server for each adapter. 
-
-	PIP_ADAPTER_INFO pAdapterInfo;
-	PIP_ADAPTER_INFO pAdapter = NULL;
-	DWORD dwRetVal = 0;
-	UINT i;
-
-	/* variables used to print DHCP time info */
-	struct tm newtime;
-	char buffer[32];
-	errno_t error;
-
-	ULONG ulOutBufLen = sizeof (IP_ADAPTER_INFO);
-	pAdapterInfo = (IP_ADAPTER_INFO *) MALLOC(sizeof (IP_ADAPTER_INFO));
-	if (pAdapterInfo == NULL) {
-		printf("Error allocating memory needed to call GetAdaptersinfo\n");
-		return 1;
+	//获取设备列表
+	if (pcap_findalldevs(&alldevs, errbuf) == -1) {
+		return false;
 	}
-	// Make an initial call to GetAdaptersInfo to get
-	// the necessary size into the ulOutBufLen variable
-	if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == ERROR_BUFFER_OVERFLOW) {
-		FREE(pAdapterInfo);
-		pAdapterInfo = (IP_ADAPTER_INFO *) MALLOC(ulOutBufLen);
-		if (pAdapterInfo == NULL) {
-			printf("Error allocating memory needed to call GetAdaptersinfo\n");
-			return 1;
-		}
+	//得到每个网卡的描述信息
+	int index = 0;
+	for(pcap_if_t* dev = alldevs; dev; dev = dev->next, ++index)
+	{
+		cout<<"ID: "<<index<<endl;
+		cout<<"Name: "<<dev->name<<endl;
+		cout<<"Description: "<<dev->description<<endl<<endl;
 	}
 
-	if ((dwRetVal = GetAdaptersInfo(pAdapterInfo, &ulOutBufLen)) == NO_ERROR) {
-		pAdapter = pAdapterInfo;
-		while (pAdapter) {
-			printf("\tComboIndex: \t5d\n", pAdapter->ComboIndex);
-			printf("\tAdapter Name: \t%s\n", pAdapter->AdapterName);
-			printf("\tAdapter Desc: \t%s\n", pAdapter->Description);
-			printf("\tAdapter Addr: \t");
-			for (i = 0; i < pAdapter->AddressLength; i++) {
-				if (i == (pAdapter->AddressLength - 1))
-					printf("%.2X\n", (int) pAdapter->Address[i]);
-				else
-					printf("%.2X-", (int) pAdapter->Address[i]);
-			}
-			printf("\tIndex: \t%d\n", pAdapter->Index);
-			printf("\tType: \t");
-			switch (pAdapter->Type) {
-						case MIB_IF_TYPE_OTHER:
-							printf("Other\n");
-							break;
-						case MIB_IF_TYPE_ETHERNET:
-							printf("Ethernet\n");
-							break;
-						case MIB_IF_TYPE_TOKENRING:
-							printf("Token Ring\n");
-							break;
-						case MIB_IF_TYPE_FDDI:
-							printf("FDDI\n");
-							break;
-						case MIB_IF_TYPE_PPP:
-							printf("PPP\n");
-							break;
-						case MIB_IF_TYPE_LOOPBACK:
-							printf("Lookback\n");
-							break;
-						case MIB_IF_TYPE_SLIP:
-							printf("Slip\n");
-							break;
-						default:
-							printf("Unknown type %ld\n", pAdapter->Type);
-							break;
-			}
-
-			printf("\tIP Address: \t%s\n",
-				pAdapter->IpAddressList.IpAddress.String);
-			printf("\tIP Mask: \t%s\n", pAdapter->IpAddressList.IpMask.String);
-
-			printf("\tGateway: \t%s\n", pAdapter->GatewayList.IpAddress.String);
-			printf("\t***\n");
-
-			if (pAdapter->DhcpEnabled) {
-				printf("\tDHCP Enabled: Yes\n");
-				printf("\t  DHCP Server: \t%s\n",
-					pAdapter->DhcpServer.IpAddress.String);
-
-				printf("\t  Lease Obtained: ");
-				/* Display local time */
-				error = _localtime32_s(&newtime, &pAdapter->LeaseObtained);
-				if (error)
-					printf("Invalid Argument to _localtime32_s\n");
-				else {
-					// Convert to an ASCII representation 
-					error = asctime_s(buffer, 32, &newtime);
-					if (error)
-						printf("Invalid Argument to asctime_s\n");
-					else
-						/* asctime_s returns the string terminated by \n\0 */
-						printf("%s", buffer);
-				}
-
-				printf("\t  Lease Expires:  ");
-				error = _localtime32_s(&newtime, &pAdapter->LeaseExpires);
-				if (error)
-					printf("Invalid Argument to _localtime32_s\n");
-				else {
-					// Convert to an ASCII representation 
-					error = asctime_s(buffer, 32, &newtime);
-					if (error)
-						printf("Invalid Argument to asctime_s\n");
-					else
-						/* asctime_s returns the string terminated by \n\0 */
-						printf("%s", buffer);
-				}
-			} else
-				printf("\tDHCP Enabled: No\n");
-
-			if (pAdapter->HaveWins) {
-				printf("\tHave Wins: Yes\n");
-				printf("\t  Primary Wins Server:    %s\n",
-					pAdapter->PrimaryWinsServer.IpAddress.String);
-				printf("\t  Secondary Wins Server:  %s\n",
-					pAdapter->SecondaryWinsServer.IpAddress.String);
-			} else
-				printf("\tHave Wins: No\n");
-			pAdapter = pAdapter->Next;
-			printf("\n");
-		}
-	} else {
-		printf("GetAdaptersInfo failed with error: %d\n", dwRetVal);
-
-	}
-	if (pAdapterInfo)
-		FREE(pAdapterInfo);
+	pcap_freealldevs(alldevs);
 
 	return true;
+}
+
+void Usage()
+{
+	std::cout<<"Wrong argument!\n";
+	std::cout<<"Usage: \n";
+	std::cout<<"-i install me\n";
+	std::cout<<"-u uninstall me\n";
+	std::cout<<"-l list devices\n";
 }
 
 int main( int argc, char* argv[])
@@ -445,18 +330,12 @@ int main( int argc, char* argv[])
 			else if (argu == "-l")
 				ListDevice();
 			else
-			{
-				std::cout<<"Wrong argument!\n";
-				std::cout<<"Usage: \n";
-				std::cout<<"-i install me\n";
-				std::cout<<"-u uninstall me\n";
-				std::cout<<"-l list devices\n";
-			}
+				Usage();
 		}
 		break;
 
 	default:
-		std::cout<<"Too many arguments!\n";
+		Usage();
 		break;
 	}
 
